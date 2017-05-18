7c75954270ad12884ddbf12038a61b2f6a214b82
Ronald Tse <ronald.tse@ribose.com>
2017-05-18 15:04:16 +0800

    Add libgsf spec and files.

---
+++ libgsf/prepare.sh
@@ -0,0 +1,19 @@
+yum install -y epel-release
+
+yum install -y automake autoconf libtool make gcc-c++ python2-devel \
+  rpmdevtools wget epel-rpm-macros
+
+yum install -y intltool gettext glib2-devel gobject-introspection \
+  libxml2-devel bzip2-devel pygobject3-devel \
+  libbonobo-devel pygtk2-devel gnome-vfs2-devel
+
+rpmdev-setuptree
+
+cd ~/rpmbuild/SOURCES/
+wget https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.41.tar.xz
+yes | cp -f /usr/local/libgsf/*.patch ~/rpmbuild/SOURCES/
+
+cd ~/rpmbuild/SPECS
+yes | cp -f /usr/local/libgsf/libgsf.spec ~/rpmbuild/SPECS
+cd ~/rpmbuild/SPECS; rpmbuild -ba libgsf.spec
+
