944f7366600e3c781d95aad454a0ec2d11144680
Ronald Tse <ronald.tse@ribose.com>
2017-05-24 08:58:06 +0800

    Rename “exmpp” to “erlang-exmpp”

--- erlang-exmpp/prepare.sh
+++ erlang-exmpp/prepare.sh
@@ -9,4 +9,4 @@ rpm --import https://github.com/riboseinc/yum/raw/master/ribose-packages.pub
 curl -L https://github.com/riboseinc/yum/raw/master/ribose.repo > /etc/yum.repos.d/ribose.repo
 yum install -y erlang-R15B01
 
-build_package exmpp
+build_package erlang-exmpp
