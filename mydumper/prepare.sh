6db1f4f51f87e522c00ef10a7ef36139f804b399
Ronald Tse <ronald.tse@ribose.com>
2017-06-29 16:38:31 +0800

    Cleanup mydumper prepare.sh.

--- mydumper/prepare.sh
+++ mydumper/prepare.sh
@@ -2,10 +2,6 @@
 
 . /usr/local/rpm-specs/setup_env.sh
 
-# yum install -y 
-# https://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
-# yum install -y mysql mysql-community-devel
-
 yum install -y cmake glib2-devel mysql-devel zlib-devel pcre-devel \
   openssl-devel python-sphinx mysql mysql-devel
 
