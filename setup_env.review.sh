16608493f5b69a4b6c75d340cc66d5963072aaa0
Jeffrey Lau <jeffrey.lau@ribose.com>
2017-07-06 10:02:43 +0800

    docker-fedora-review: Make it print out instructions
    ... since mock wouldn't run if we do them for you.

---
+++ setup_env.review.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+install_review_packages() {
+  dnf -y install yum-utils fedora-review fedora-packager sudo
+}
+
+install_review_packages
+
+# echo root | passwd root --stdin # For debugging purposes
+useradd reviewer -p reviewer
+usermod -a -G mock reviewer
+
+echo
+echo -e "# Run: \e[1mfedora-review\e[m -b <bug number>"
+echo -e "#  or: \e[1mfedora-review\e[m -n ${1##*/}"
+echo
+echo -e "# Copy & paste the following into the terminal:"
+echo -e "\e[1m"
+echo -e "su - reviewer"
+echo -e "cd $1"
+echo -e "cp *.src.rpm *.spec ~"
+echo -e "cd"
+echo -e "fedora-review -n ${1##*/}"
+echo -e "\e[m"
+
+exec bash
+
+# vim:et:sw=2:sts=2:ts=2
