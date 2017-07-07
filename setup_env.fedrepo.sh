0986b8b25d98cdb7c6841ba2df0662648927d731
Jeffrey Lau <jeffrey.lau@ribose.com>
2017-07-07 17:14:24 +0800

    docker-*: Make it more user-friendly

--- setup_env.fedrepo.sh
+++ setup_env.fedrepo.sh
@@ -1,26 +1,51 @@
 #!/bin/bash -x
 
 install_packages() {
-  dnf -y install yum-utils fedora-packager sudo
+  dnf -y install yum-utils fedora-packager sudo man
 }
 
-install_packages
+readonly faslogin="${1}"
+readonly package_name="${2}"
 
-# echo root | passwd root --stdin # For debugging purposes
-useradd packager -p packager
+usage() {
+  echo -e "# \e[1mfedrepo-req\e[m"
+  echo
+  echo -e "# Copy & paste the following into the terminal:"
+  echo -e "\e[1m"
+  echo -e "su - packager"
+  echo -e ". /usr/local/setup.sh"
+  echo -e "fedpkg --user ${faslogin} clone ${package_name-package_name}"
+  cat > /usr/local/setup.sh <<EOF
+export PS1='[\u@\h \W\$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")]\$ '
+cp /usr/local/.gitconfig ~
+mkdir ~/.ssh ; cp /usr/local/.ssh/* ~/.ssh
+mkdir ~/fedora-scm ; cd ~/fedora-scm
+EOF
 
-echo -e "# \e[1mfedrepo-req\e[m"
-echo
-echo -e "# Copy & paste the following into the terminal:"
-echo -e "\e[1m"
-echo -e "su - packager"
-echo -e "mkdir ~/.ssh ; cp /usr/local/.ssh/* ~/.ssh"
-echo -e "cp -r /usr/local/fedrepo_req ~"
-echo -e "cd ~/fedrepo_req"
-echo -e "python setup.py build"
-echo -e "python setup.py install"
-echo -e "fedrepo-req ${1} -t ${2}"
-echo -e "\e[m"
+  # echo
+  # echo -e "cp -r /usr/local/fedrepo_req ~"
+  # echo -e "cd ~/fedrepo_req"
+  # echo -e "python setup.py build"
+  # echo -e "python setup.py install"
+  # echo -e "fedrepo-req ${1} -t ${2}"
+  echo -e "\e[m"
+  echo -e "Type \e[1musage\e[m for this help message."
+  echo -e "Type \e[1minstall_packages\e[m to install prerequisites."
+}
+
+export -f usage
+export -f install_packages
+
+setup() {
+  install_packages
+
+  # echo root | passwd root --stdin # For debugging purposes
+  useradd packager -p packager
+}
+
+setup
+
+usage
 
 exec bash
 
--- setup_env.review.sh
+++ setup_env.review.sh
@@ -4,25 +4,41 @@ install_packages() {
   dnf -y install yum-utils fedora-review sudo
 }
 
-install_packages
+readonly package_path="${1}"
+readonly package_name="${package_path##*/}"
 
-# echo root | passwd root --stdin # For debugging purposes
-useradd reviewer -p reviewer
-usermod -a -G mock reviewer
+usage() {
+  echo
+  echo -e "# \e[1mfedora-review\e[m"
+  echo -e "# Run: \e[1mfedora-review\e[m -b <bug number>"
+  echo -e "#  or: \e[1mfedora-review\e[m -n ${package_name}"
+  echo
+  echo -e "# Copy & paste the following into the terminal:"
+  echo -e "\e[1m"
+  echo -e "su - reviewer"
+  echo -e "cd ${package_path}"
+  echo -e "cp *.src.rpm *.spec ~"
+  echo -e "cd"
+  echo -e "fedora-review -n ${package_name}"
+  echo -e "\e[m"
+  echo -e "Type \e[1musage\e[m for this help message."
+  echo -e "Type \e[1minstall_packages\e[m to install prerequisites."
+}
 
-echo
-echo -e "# \e[1mfedora-review\e[m"
-echo -e "# Run: \e[1mfedora-review\e[m -b <bug number>"
-echo -e "#  or: \e[1mfedora-review\e[m -n ${1##*/}"
-echo
-echo -e "# Copy & paste the following into the terminal:"
-echo -e "\e[1m"
-echo -e "su - reviewer"
-echo -e "cd $1"
-echo -e "cp *.src.rpm *.spec ~"
-echo -e "cd"
-echo -e "fedora-review -n ${1##*/}"
-echo -e "\e[m"
+export -f usage
+export -f install_packages
+
+setup() {
+  install_packages
+
+  # echo root | passwd root --stdin # For debugging purposes
+  useradd reviewer -p reviewer
+  usermod -a -G mock reviewer
+}
+
+setup
+
+usage
 
 exec bash
 
