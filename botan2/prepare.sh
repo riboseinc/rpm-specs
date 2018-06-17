b7a70ec8956f2d593cca11b697fbb2f64b329325
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2018-06-16 20:34:23 -0400

    Add botan2-2.6.0.

--- botan2/prepare.sh
+++ botan2/prepare.sh
@@ -3,6 +3,6 @@
 . /usr/local/rpm-specs/setup_env.sh
 
 yum install -y openssl-devel zlib-devel bzip2-devel \
-  boost-devel which python-devel doxygen
+  python34-devel python-sphinx
 
-build_package botan
+build_package botan2
