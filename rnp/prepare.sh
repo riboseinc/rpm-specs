9a1faea53d4140a7bd26d443d918af4e7edaf32f
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2018-06-22 22:15:32 -0400

    Fix up rnp rpm.

--- rnp/prepare.sh
+++ rnp/prepare.sh
@@ -1,49 +1,28 @@
 #!/bin/bash
-
+set -x
 # See https://github.com/riboseinc/rnp/blob/master/doc/PACKAGING.md
+
+VERSION=0.9.0
 
 . /usr/local/rpm-specs/setup_env.sh
 
-# build_cmake_package() {
-#   local readonly package_name="${1}"
-#
-#   rpmdev-setuptree
-#   yes | cp -a /usr/local/${package-name}/* ~/rpmbuild/SOURCES
-#
-#   # spectool -g -R ${spec_dest}
-#   rpmbuild --define "_topdir <path_to_build_dir>" --rebuild <SRPM_file_name>
-#   rpmbuild ${RPMBUILD_FLAGS:--v -ba} ${spec_dest} || \
-#     {
-#       echo "rpmbuild failed." >&2;
-#       [ $CI ] && exit 1
-#       if [ "$(launched_from)" != "bash" ]; then
-#         echo "Now yielding control to bash." >&2 && \
-#         exec bash
-#       fi
-#     }
-# }
-
-yum -y install bzip2-devel zlib-devel libcmocka-devel libstdc++-static
+yum -y install bzip2-devel zlib-devel libstdc++-static
 yum -y install botan2-devel json-c-devel
 ln -s /usr/bin/cmake3 /usr/bin/cmake
 ln -s /usr/bin/cpack3 /usr/bin/cpack
 
 rpmdev-setuptree
-cd ~/rpmbuild/SOURCES/
-git clone https://github.com/riboseinc/rnp/ .
 
-# rpmbuild --define "_topdir <path_to_build_dir>" --rebuild <SRPM_file_name>
+curl -LO https://github.com/riboseinc/rnp/archive/v${VERSION}.tar.gz
+tar -xzf v${VERSION}.tar.gz
 
-cd ~/rpmbuild/SOURCES/rnp
-
-cmake -DBUILD_SHARED_LIBS=on -DBUILD_TESTING=off -DCPACK_GENERATOR=RPM .
+cmakeopts=(-DBUILD_SHARED_LIBS=on -DBUILD_TESTING=off)
+cmake "${cmakeopts[@]}" -DCPACK_RPM_SOURCE_PKG_BUILD_PARAMS="${cmakeopts[*]}" -DCPACK_GENERATOR=RPM rnp-${VERSION}
 cpack -G RPM --config ./CPackSourceConfig.cmake
-make package
 
-mv *.src.rpm ~/rpmbuild/SRPMS/
-# mv *.noarch.rpm ~/rpmbuild/RPMS/noarch/
-mv *.x86_64.rpm ~/rpmbuild/RPMS/x86_64/
+rpmdev-extract rnp-${VERSION}-*.src.rpm
+mv rnp-${VERSION}-*.src/*.spec .
+mkdir -p sources
+mv rnp-${VERSION}-*.src/rnp-${VERSION}.tar.gz sources
 
-yum install -y ~/rpmbuild/RPMS/x86_64/*.rpm
-
-# bash
+build_package rnp
